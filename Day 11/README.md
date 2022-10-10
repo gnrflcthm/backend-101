8014ec8351ec2f35f06bd2ef5ac3633f5419535c
ney.owo <72762670+rldyen@users.noreply.github.com>
2022-10-10 21:02:08 +0800

    updated day 11

---
+++ Day 11/README.md
@@ -0,0 +1,7 @@
+# **Backend Development 101: Day 11**
+
+Run first node seeds.js
+
+then
+
+node index.js
---
+++ Day 11/mongoose_express/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+const mongoose = require('mongoose');
+const Product = require ("./models/product");
+
+mongoose.connect('mongodb://localhost:27017/farmStand')
+.then(() => {
+    console.log("Connection Open");
+})
+.catch(err =>{
+    console.log("Error!");
+    console.log(err);
+})
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/products', async (req,res) =>{
+    const products = await Product.find({});
+    res.render('products/index', {products});
+});
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render("products/show", { product });
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.`)
+})
